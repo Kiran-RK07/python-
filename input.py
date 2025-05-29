dd5556c9e0219c37c9f78e54a5b5f5668feebda0
Kiran-RK07 <kiranrk0009@gmail.com>
2025-05-29 22:16:09 +0530

    input function

---
+++ input.py
@@ -0,0 +1,10 @@
+#input() = It always returns data as a String
+
+name = input("What is your name? ")
+age = int(input("How old are you? ")) #If we want number to be included we can use "int" in the starting.
+
+age = age + 1
+
+print(f"Hello {name}! ")
+print("HAPPY BRITHDAY!!")
+print(f"Your {age} year old")
