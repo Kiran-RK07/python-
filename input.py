b28836951a1e19967a99428a5e4a5e775b58983a
Kiran-RK07 <kiranrk0009@gmail.com>
2025-05-29 22:44:52 +0530

    Update input.py

--- input.py
+++ input.py
@@ -8,3 +8,12 @@ age = age + 1
 print(f"Hello {name}! ")
 print("HAPPY BRITHDAY!!")
 print(f"Your {age} year old")
+
+#Exersice Calculate Area of Rectangle.
+
+length = float(input("Enter the lenght? "))
+width = float(input("Enter the width? "))
+
+total = length * width
+
+print(f"Area of Rectangle is : {total}cm²") #To Get the sqaure sign Numlock must on and press Alt + 0178
